19669b75b8d5c8bdc71da0ee7387a9d014ab0025
Eugenia Revol <euge.revol@gmail.com>
2022-09-25 20:53:21 -0300

    update eslintrc

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex justify-center">
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+    </div>
   );
 }
 
